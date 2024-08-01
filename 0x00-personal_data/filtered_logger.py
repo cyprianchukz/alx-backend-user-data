c640b1b41122571f408298440896981927bf1abe
cyprianchukz <cyprianchukz@gmail.com>
2024-08-01 16:55:04 +0100

    done

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A module for filtering logs.
+"""
+import os
+import re
+import logging
+import mysql.connector
+from typing import List
+
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
